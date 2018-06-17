e2de551803f4f0ea6b2a6fd496c08ddf00f2eb09
JacksonSui <jiashui@hotmail.com>
2018-06-17 22:57:44 +0800

    clean up proj

--- Parser.h
+++ Parser.h
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <algorithm>
 #include "Document.h"
 #include "Node.h"
 
 class Parser{
     private:
-        std::string fileName;
         std::string content;
         std::ifstream in;
         CDocument cd;
